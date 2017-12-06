2575ee6985f5e515d0e1fec9b099e2dd22cf0349
matthewoden <heymatthewoden@gmail.com>
2017-12-05 19:42:00 -0600

    day 4

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -1,7 +1,7 @@
 defmodule AoC do
   @moduledoc """
   Documentation for AdventOfCode2017.
-  """  
+  """
 
   def stream!(file) do
       File.stream!("./resources/#{file}")
@@ -11,7 +11,32 @@ defmodule AoC do
     File.read!("./resources/#{file}")
   end
 
-  def pmap (enum) do
-    
+  def pmap(collection, func) do
+    collection
+    |> Enum.map(&Task.async(fn -> func.(&1) end))
+    |> Enum.map(&Task.await/1)
+  end
+
+
+  def read_line(line, seperator, state \\ "", func \\ fn (f) -> f end) do
+    Stream.map(line, &line_reader(&1, "", seperator, state, func))
+  end
+
+  def line_reader(_, _, _, {:exit, state}, _), do: state
+  def line_reader(input, word, seperator, state, func) do
+      case input do
+          "" ->
+              state
+
+        <<value, tail :: binary >>  when value in [seperator, ?\n] ->
+              state = func.(word, state)
+
+            line_reader(tail, "", seperator, state, func)
+
+        <<head ::size(8), tail :: binary >> ->
+              word = << word :: binary, head :: size(8) >>
+
+            line_reader(tail, word, seperator, state, func)
+      end
   end
 end
--- lib/day4.ex
+++ lib/day4.ex
@@ -1,9 +1,53 @@
 defmodule AoC.Day4 do
+    
+    @moduledoc """
+    Moved The linereader from day 2 into it's own module. Now we have a word
+    reducer!
+
+    For part one, checked if the key existed.
+    For part two, abstracted the key check to a composable call, and wrote a
+    recursive quicksort.
+
+    """
+
+    #binary quicksort!
+    def sort(""), do: ""
+    def sort(<< head :: utf8, tail :: binary >>) do
+        {lesser, greater} = partition(tail, head, { "", "" })
+        << sort(lesser) :: binary, head :: utf8, sort(greater) :: binary >>
+    end
+
+    def partition("", _value, result), do: result
+    def partition(<< head :: utf8, tail :: binary >>, value, {greater, lesser}) do
+        cond do
+            value > head -> partition(tail, value, {<< head :: utf8, greater :: binary, >>, lesser})
+            true -> partition(tail, value, {greater, << lesser :: binary, head :: utf8 >>})
+        end
+    end
+
+    # make a unique map, or exit.
+    def unique_map(value, map) do
+        if Map.has_key?(map, value) do
+            true -> {:exit, false}
+            _ ->    Map.put(map, value, true)
+        end
+    end
+
+    #-------------------------------------
+
+    @sep 32 # space charcode
     def part_1 do
-        
+        AoC.stream!("day4-1")
+        |> AoC.read_line(@sep, %{}, fn (curr, map) -> unique_map(curr, map) end)
+        |> Stream.filter(fn x -> x end)
+        |> Enum.count()
     end
 
     def part_2 do
-        
+        AoC.stream!("day4-1")
+        |> AoC.read_line(@sep, %{}, fn (curr, map) -> unique_map(sort(curr), map) end)
+        |> Stream.filter(fn x -> x end)
+        |> Enum.count()
     end
+
 end
